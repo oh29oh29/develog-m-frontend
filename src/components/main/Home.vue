421ad2854b83298e4f052c65220f84b550858030
oh29oh29 <hyukjae89@gmail.com>
2019-02-17 17:42:53 +0900

    메인 - 카테고리 router-view 추가

---
+++ src/components/main/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header></Header>
+    <div class="content-wrap">
+      <router-view name="category"></router-view>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+
+export default {
+  name: 'Home',
+  components: { Header }
+}
+</script>
+
+<style scoped>
+.content-wrap {
+  width: 960px;
+  margin: 0 auto;
+}
+</style>
