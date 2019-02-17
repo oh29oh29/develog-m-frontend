83d1c8305694f0d9fcdb7c0775b8f96ab9f206bc
oh29oh29 <hyukjae89@gmail.com>
2019-02-17 17:42:22 +0900

    메인 - 카테고리 및 포스트 컴포넌트 수정

---
+++ src/components/main/post/Category.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="category-wrap">
+    <div class="category" v-for="category in categories" v-bind:key="category.id">
+      <span>{{ category.name }}</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  data () {
+    return {
+      categories: []
+    }
+  },
+  created () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+      const _this = this
+      this.$http.get('/category')
+        .then(result => {
+          console.log(result)
+          _this.categories = result.data
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.category-wrap {
+  display: inline-block;
+  vertical-align: top;
+  width: 200px;
+  margin: 0 20px;
+  font-size: 16px;
+}
+.category {
+
+}
+</style>
---
+++ src/components/main/post/List.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="post-list-wrap">
+    <article class="post" v-for="post in posts" v-bind:key="post.id">
+      <span>{{ post.title }}</span>
+      <span>{{ post.regDate }}</span>
+      <p>{{ post.description }}</p>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  created () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+      const _this = this
+      this.$http.get('/post')
+        .then(result => {
+          console.log(result)
+          _this.posts = result.data
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-list-wrap {
+  display: inline-block;
+  width: 680px;
+  margin: 0 20px;
+  font-size: 16px;
+}
+.post {
+
+}
+</style>
