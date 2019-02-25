d407471cf46e3d41d90263906dc256fa71719e64
oh29oh29 <hyukjae89@gmail.com>
2019-02-25 20:57:37 +0900

    indent 수정

--- src/mock/MockContainer.js
+++ src/mock/MockContainer.js
@@ -3,22 +3,22 @@ import { categories } from "./responseData/categories";
 import { postMockData } from "./responseData/posts";
 
 export class MockContainer {
-    constructor (axios) {
-        this.mock = new MockAdapter(axios);
-        this.createResponse();
-    }
+  constructor (axios) {
+    this.mock = new MockAdapter(axios);
+    this.createResponse();
+  }
 
-    createResponse () {
-        this.mock.onGet('/category').reply(200, categories);
-        this.mock.onPost('/category').reply(200);
-        this.mock.onPatch('/category').reply(200);
-        this.mock.onDelete('/category').reply(200);
-        this.mock.onGet('/JAVA/1').reply(200, postMockData.postMockData1);
-        this.mock.onGet('/Spring 프레임워크').reply(200, postMockData.postMockData2);
-        this.mock.onGet('/JavaScript').reply(200, postMockData.postMockData3);
-        this.mock.onGet('/Vuejs').reply(200, postMockData.postMockData4);
-        this.mock.onGet('/Tomcat').reply(200, postMockData.postMockData5);
-        this.mock.onGet('/JAVA/101').reply(200, postMockData.postMockData1[0]);
-        this.mock.onGet(new RegExp(`/*`)).reply(200, postMockData.postTotalMockData);
-    }
+  createResponse () {
+    this.mock.onGet('/category').reply(200, categories);
+    this.mock.onPost('/category').reply(200);
+    this.mock.onPatch('/category').reply(200);
+    this.mock.onDelete('/category').reply(200);
+    this.mock.onGet('/JAVA/1').reply(200, postMockData.postMockData1);
+    this.mock.onGet('/Spring 프레임워크').reply(200, postMockData.postMockData2);
+    this.mock.onGet('/JavaScript').reply(200, postMockData.postMockData3);
+    this.mock.onGet('/Vuejs').reply(200, postMockData.postMockData4);
+    this.mock.onGet('/Tomcat').reply(200, postMockData.postMockData5);
+    this.mock.onGet('/JAVA/101').reply(200, postMockData.postMockData1[0]);
+    this.mock.onGet(new RegExp(`/*`)).reply(200, postMockData.postTotalMockData);
+  }
 }
